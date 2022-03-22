9d44e106250291821894361b6c52c6def4963fe6
mo <mohamedabidi9@hotmail.fr>
2022-03-22 13:39:43 +0100

    new pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,9 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
-    }
+    },
+    'océanéa': {
+        'ingredients': {'mozzarella': 3, 'thon': 4, 'capre': 3},
+        'price': 11,
+    },
 }
